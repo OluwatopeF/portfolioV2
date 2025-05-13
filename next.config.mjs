2528b3c4cd94fbedf3b5321322e1152efb4c9d4e
OluwatopeF <topeumar@gmail.com>
2025-05-12 23:17:53 -0400

    update 5/12/2025

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV = 'development';
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+    reactStrictMode: false,
+    trailingSlash: true,
+    assetPrefix: isProd ? '/portfolioV2/' : '',
+    basePath: isProd ? '/portfolioV2':'',
     output: 'export',
 };
 
 export default nextConfig;
-
-
-    // reactStrictMode: false,
-    // trailingSlash: true,
-    // assetPrefix: isProd ? '/portfolioV2/' : '',
-    // basePath: isProd ? '/portfolioV2' : '',
-    // output: 'export',
